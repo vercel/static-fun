447d5a89beb243a5d924ff7f3d5d33022b5d4dd3
Allen Hai <allenhai03@gmail.com>
2019-08-26 12:57:35 -0500

    set default null status if no subdomain is requested

--- pages/index.js
+++ pages/index.js
@@ -3,79 +3,84 @@ import Head from "next/head";
 import PacmanLoader from "react-spinners/PacmanLoader";
 
 export default function IndexPage() {
-  const [pageData, setPageData] = useState(undefined)
+  const [pageData, setPageData] = useState(undefined);
   useEffect(() => {
     async function getPageData() {
       let page = window.location.host.split(".")[0];
-      let res = await fetch(`/api/get-page?page=${page}`);
-      let json = await res.json();
-      setPageData(json.pageData)
+      if (page.length === 3) {
+        let res = await fetch(`/api/get-page?page=${page}`);
+        let json = await res.json();
+        setPageData(json.pageData);
+      } else {
+        setPageData(null);
+      }
     }
     getPageData();
   }, []);
-  if (typeof pageData === 'undefined') {
-    return <main>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        />
-        <meta
-          name="description"
-          content="claim a wildcard subdomain and have fun!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Static Fun</title>
-      </Head>
-      <PacmanLoader loading={typeof pageData === 'undefined'} />
-      <style jsx>{`
-        main {
-          position: fixed;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-      `}</style>
-    </main>
+  if (typeof pageData === "undefined") {
+    return (
+      <main>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          />
+          <meta
+            name="description"
+            content="claim a wildcard subdomain and have fun!"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Static Fun</title>
+        </Head>
+        <PacmanLoader loading={typeof pageData === "undefined"} />
+        <style jsx>{`
+          main {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+        `}</style>
+      </main>
+    );
   } else {
-    
-  return (
-    <main>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        />
-        <meta
-          name="description"
-          content="claim a wildcard subdomain and have fun!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Static Fun</title>
-      </Head>
-      <p>Welcome to static.fun</p>
-      <style jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-        }
-        main {
-          position: fixed;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          font-size: 62.5%;
-          font-size: 1.6rem;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
-            Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
-            "Segoe UI Symbol";
-          background: #fff;
-          color: #000;
-        }
-      `}</style>
-    </main>
-  )
+    return (
+      <main>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          />
+          <meta
+            name="description"
+            content="claim a wildcard subdomain and have fun!"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Static Fun</title>
+        </Head>
+        <p>Welcome to static.fun</p>
+        <style jsx>{`
+          * {
+            margin: 0;
+            padding: 0;
+          }
+          main {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 62.5%;
+            font-size: 1.6rem;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+              Helvetica, Arial, sans-serif, "Apple Color Emoji",
+              "Segoe UI Emoji", "Segoe UI Symbol";
+            background: #fff;
+            color: #000;
+          }
+        `}</style>
+      </main>
+    );
   }
 }

e88561b2fa4c44d67413638118c329caa69cb280
Allen Hai <allenhai03@gmail.com>
2019-09-10 04:19:39 -0500

    set modal to default state

--- components/editor.tsx
+++ components/editor.tsx
@@ -6,7 +6,7 @@ import SaveBar from "./save-bar";
 export default function EditorContainer({ html, email, editLink }) {
   const [_html, setHtml] = useState(html || "");
   const [_email, setEmail] = useState(email);
-  const [dialogOpen, setDialogOpen] = useState(true);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   return (
     <div className="root-editor-container">

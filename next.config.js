55d0fe0a64e3f02c1eb87d49d85d07f3bd6e69ae
Allen Hai <allenhai03@gmail.com>
2019-09-10 03:47:31 -0500

    add pusher deps

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    PUSHER_APP_KEY: process.env.PUSHER_APP_KEY
+  }
+};
--- views/static-layout.tsx
+++ views/static-layout.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 import Head from "next/head";
+
 export function RenderStaticLayout({ html }) {
+  const [newHtml, setNewHtml] = useState();
+
+  function hydrateHtml() {}
+
+  useEffect(() => {
+    const pusher = new Pusher(process.env.PUSHER_APP_KEY, {
+      cluster: "us2"
+    });
+  }, []);
+
   return (
     <>
       <Head>
         <title>Static Fun</title>
         <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
       </Head>
-      <main dangerouslySetInnerHTML={{ __html: html }}></main>
+      <main dangerouslySetInnerHTML={{ __html: newHtml || html }}></main>
       <style jsx>{`
         main {
           width: 100%;
